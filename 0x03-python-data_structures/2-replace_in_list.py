c692b92179b0278568bdb374e16590eba16f3f49
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-12-06 13:35:00 -0800

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx > length - 1:
         return (my_list)
-    my_list[idx] = elemenit
+    my_list[idx] = element
     return (my_list)
